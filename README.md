5a2efe58ed8bb80e44b32ec4d1cf68b09e1d90d0
tathao <tathaonguyen@gmail.com>
2022-08-19 00:20:21 +0700

    Udacity Project 2

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# udacity-project2
-## the function to get image response
-#3 fix bug "Cannot find MIME for Buffer<null>  (Thanks nhannt20@fsoft.com.vn for supporing)
+# Udagram Image Filtering Microservice
 
+## The function to get the image URL filter response
+## Fix bug Could not find MIME for Buffer<null> when call filterImageFromURL function. (Thanks nhannt20@fsoft.com.vn for supporting)
+## The image URL: http://haont1-project2-dev.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/b/bd/Golden_tabby_and_white_kitten_n01.jpg
+## The Elastic Beanstalk dashboard screenshot
---
+++ src/server.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  app.get("/filteredimage", async(req:express.Request, res:express.Response)=>{
+    let {image_url} = req.query;
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+    if(!image_url) {
+      return res.status(400).send(`Missing the image URL`)
+    }
+  //    2. call filterImageFromURL(image_url) to filter the image
+    let filterImageFromUrl = await filterImageFromURL(image_url.toString());
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+    return res.status(200).sendFile(filterImageFromUrl,()=>{
+      deleteLocalFiles([filterImageFromUrl]);
+    });
+
+  /**************************************************************************** */
+});
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
